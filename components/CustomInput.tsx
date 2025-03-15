8a92dc56efebae58eae508004a32f945d2b22ca1
andresfelipediaz782@gmail.com <andresfelipediaz782@gmail.com>
2025-03-14 22:46:56 -0500

    feat: edicion modulo registro

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,23 +1,48 @@
 import React from "react";
-import { TextInput, StyleSheet, TextInputProps } from "react-native";
+import { View, TextInput, StyleSheet, TextInputProps, Platform } from "react-native";
+import Feather from "react-native-vector-icons/Feather"; 
 import { colors } from "../config/theme";
 
-interface CustomInputProps extends TextInputProps {}
+interface CustomInputProps extends TextInputProps {
+  iconName?: string;
+}
 
-export default function CustomInput(props: CustomInputProps) {
-  return <TextInput {...props} style={[styles.input, props.style]} />;
+export default function CustomInput({ iconName, ...props }: CustomInputProps) {
+  return (
+    <View style={styles.container}>
+      {iconName && (
+        <Feather name={iconName} size={20} color={colors.gray} style={styles.icon} />
+      )}
+      <TextInput
+        {...props}
+        style={styles.input}
+        placeholderTextColor={colors.gray}
+        underlineColorAndroid="transparent" // Evita la línea azul en Android
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
     width: "100%",
     height: 50,
-    padding: 15,
+    paddingHorizontal: 15,
     borderWidth: 1,
     borderColor: colors.border,
     borderRadius: 15,
     backgroundColor: colors.white,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
     fontSize: 16,
     color: colors.darkGray,
+    borderWidth: 0, // Elimina borde en iOS
+    outlineStyle: "none", // Evita borde en Android
   },
 });
